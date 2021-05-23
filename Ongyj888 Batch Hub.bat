fa8cf3dc7d8e67b0498c05a247ad11c7a891b728
Ongyj888 <67620716+Ongyj888@users.noreply.github.com>
2021-05-23 17:30:50 +0800

    Add files via upload

---
+++ Ongyj888 Batch Hub.bat
@@ -0,0 +1,291 @@
+@echo off
+
+
+REM Maximize the console window (important)
+mode 200,70
+REM Command at script starting
+chcp 65001
+color F0
+setlocal enabledelayedexpansion
+set loadingtime=1
+set version=v1.0
+set menustate=Starting
+title %menustate% - Ongyj888 batch hub! 
+
+REM Command attributes
+if ["%1"] == [""] goto start
+if "%1" == "/start" goto start
+if "%1" == "/debug" goto debug
+if "%1" == "/help" goto help
+if "%1" == "/menu" goto menu
+goto help
+
+REM Help message
+:help
+echo ********************************************
+echo *            A r g u m e n t s             *
+echo ********************************************
+echo *   /start    * Start hub normally         *
+echo *   /debug    * Show debug file            *
+echo *   /menu     * Enter menu directly        *
+echo *   /help     * Show this message          *
+echo ********************************************
+pause
+exit
+
+REM Start at script execute or /start argument detected
+:start
+cls
+echo Please maximize the console window for greater experience
+echo.
+echo Press any key to start.
+pause > nul 
+goto ani
+
+REM Start Animation
+:ani
+REM 1
+cls
+echo.
+echo.
+echo.
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+ping 1.1.1.1 -n 1 -w 1000 >nul                                                     
+
+REM 2
+cls
+echo.
+echo.
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 3
+cls
+echo.
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 4
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 5
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+ping 1.1.1.1 -n 1 -w 1000 >nul
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 6
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _
+ping 1.1.1.1 -n 1 -w 1000 >nul
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 7
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo __
+ping 1.1.1.1 -n 1 -w 1000 >nul
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 8
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo ___
+ping 1.1.1.1 -n 1 -w 1000 >nul
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 9
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo ____
+ping 1.1.1.1 -n 1 -w 1000 >nul
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 10
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____
+ping 1.1.1.1 -n 1 -w 1000 >nul
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 11
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____L
+ping 1.1.1.1 -n 1 -w 1000 >nul
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 12
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____Lo
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 13
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____Loa
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 14
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____Load
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 15
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____Loadi
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 16
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____Loadin
+ping 1.1.1.1 -n 1 -w 1000 >nul
+
+REM 17
+cls
+echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+echo.
+echo _____Loading
+ping 1.1.1.1 -n 1 -w 1000 >nul
+goto loadingloop
+
+REM Loop loading animation 5 times abd go to menu
+:loadingloop
+if %loadingtime% leq 5 (
+    goto loadloop
+) else (
+        goto menu
+)
+
+REM Loading animation
+:loadloop
+    cls
+    echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+    echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+    echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+    echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+    echo.
+    echo _____Loading.
+    ping timeout /t 1 /nobreak > nul
+    cls
+    echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+    echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+    echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+    echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+    echo.
+    echo _____Loading..
+    ping timeout /t 1 /nobreak > nul
+    cls
+    echo  ____  _  _    _____  _  _  ___  _  _  ____  ___  ___  ___ 
+    echo ^(  _ \( \/ )  (  _  )( \( )/ __)( \/ )(_  _)( _ )( _ )( _ )
+    echo  ^) _ ^< \  /    )(_)(  )  (( (_-. \  /.-_)(  / _ \/ _ \/ _ \
+    echo (____/ (__)   (_____)(_)\_)\___/ (__)\____) \___/\___/\___/
+    echo.
+    echo _____Loading...
+    ping timeout /t 1 /nobreak > nul
+    set /a loadingtime=%loadingtime%+1
+    goto loadingloop
+
+REM Main Menu
+:menu
+cls
+set menustate=Main Menu
+title %menustate% - Ongyj888 batch hub!
+echo d8888b.  .d8b.  d888888b  .o88b. db   db     db   db db    db d8888b. 
+echo 88  `8D d8' `8b `~~88~~' d8P  Y8 88   88     88   88 88    88 88  `8D 
+echo 88oooY' 88ooo88    88    8P      88ooo88     88ooo88 88    88 88oooY' 
+echo 88~~~b. 88~~~88    88    8b      88~~~88     88~~~88 88    88 88~~~b. 
+echo 88   8D 88   88    88    Y8b  d8 88   88     88   88 88b  d88 88   8D 
+echo Y8888P' YP   YP    YP     `Y88P' YP   YP     YP   YP ~Y8888P' Y8888P' 
+echo.
+echo +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-                                                    
+echo.       
+echo +-+-+-+-+ +-+-+-+-+
+echo ^|M^|a^|i^|n^| ^|M^|e^|n^|u^|
+echo +-+-+-+-+ +-+-+-+-+
+echo.
+echo 1. (Coming soon)
+echo 2. (Coming soon)
+echo 3. Pranks
+echo 4. Tools 
+echo 5. Settings
+echo 6. Credits
+echo 7. Debug (For devoloper only)
+echo 8. Exit
+set /p m1=^>
+if x1 == 1 goto comingsoon
+echo Your input: %m1% is not vaild.
+echo Press any key to retry.
+pause > nul
+goto menu
+                                                     
+                                              
+                                                                                                                                                              
+                                                                                                                            
+pause
+
+
